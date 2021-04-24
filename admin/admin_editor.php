465b77ee837baf8a66d1320b7cc3a810e9d4140f
Sutha Binkamit <42909437+sutha001@users.noreply.github.com>
2021-04-25 00:14:02 +0700

    admin_editor templat

--- admin/admin_editor.php
+++ admin/admin_editor.php
@@ -1,14 +1,44 @@
-<?php include '../connect.php'?>
+<?php include '../connect.php' ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_all_editor.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <title>Editor</title>
 </head>
+
 <body>
-    
+    <div class="area_all" style="background-color: black;">
+        <div class="menu_editor">
+            <div class="row_edit">
+                <button class="btn btn-dark" style="background-color: #ffffff; color:#1b221b;">การจัดการ</button>
+            </div>
+            <div class="row_edit">
+                <button class="btn btn-dark" style="background-color: #4f4f4f;">Order</button>
+            </div>
+            <div class="row_edit">
+                <button class="btn btn-dark" style="background-color: #4f4f4f;">Stock</button>
+            </div>
+            <div class="row_edit">
+                <button class="btn btn-dark" style="background-color: #4f4f4f;">เพิ่มสินค้า</button>
+            </div>
+            <div class="row_edit">
+                <button class="btn btn-dark" style="background-color: #4f4f4f;">การจัดการผู้ใช้</button>
+            </div>
+        </div>
+
+        <div class="other_editor">
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
 </body>
+
 </html>
