f57bee71eab1e701b65340bbf264421447de62fb
Rachata.p <61070348@it.kmitl.ac.th>
2021-05-04 15:05:23 +0700

    แก้

--- index-fix.php
+++ index-fix.php
@@ -18,7 +18,7 @@ $result = mysqli_query($connect, $sql);
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
   <link rel="stylesheet" href="/path/to/flickity.css" media="screen">
-  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="user/New folder/main.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&family=Taviraj&display=swap" rel="stylesheet"> 
   <title>HousePlantEcom</title>
@@ -88,7 +88,7 @@ $result = mysqli_query($connect, $sql);
     </a>
   </div>-->
   <div class="head"style="height: auto; display: flex; align-items: center; justify-content: center;">
-    <img src="img/logo.png" width="45%">
+    <img src="user/New folder/img/logo.png" width="45%">
   </div>
   <!--  -->
   <div class="container">
